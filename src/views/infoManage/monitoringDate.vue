84e9c92d26d6d3407cae865c4f9dae9f904c578b
xiangguo <xiangguo@bao.cn>
2019-09-22 16:17:38 +0800

    s

--- src/views/infoManage/monitoringDate.vue
+++ src/views/infoManage/monitoringDate.vue
@@ -5,15 +5,15 @@
   <bottom-data/>
   <div class="bottomBar">
     <div class="tabs">
-      <div class="tab">Z方向测点信息</div>
-      <div class="tab">调查报告Sheet</div>
-      <div class="tab">外观变形测点TP1</div>
-      <div class="tab">外观变形测点TP2</div>
-      <div class="tab">外观变形测点TP3</div>
-      <div class="tab">纵向测点信息</div>
-      <div class="tab">调查样本监测</div>
-      <div class="tab">调度室监控录像</div>
-      <div class="tab">VR虚拟演示</div>
+      <div :class="['tab',selectIndex===1&&'active']" @click="handleChangeTab(1)">Z方向测点信息</div>
+      <div :class="['tab',selectIndex===2&&'active']" @click="handleChangeTab(2)">调查报告Sheet</div>
+      <div :class="['tab',selectIndex===3&&'active']" @click="handleChangeTab(3)">外观变形测点TP1</div>
+      <div :class="['tab',selectIndex===4&&'active']" @click="handleChangeTab(4)">外观变形测点TP2</div>
+      <div :class="['tab',selectIndex===5&&'active']" @click="handleChangeTab(5)">外观变形测点TP3</div>
+      <div :class="['tab',selectIndex===6&&'active']" @click="handleChangeTab(6)">纵向测点信息</div>
+      <div :class="['tab',selectIndex===7&&'active']" @click="handleChangeTab(7)">调查样本监测</div>
+      <div :class="['tab',selectIndex===8&&'active']" @click="handleChangeTab(8)">调度室监控录像</div>
+      <div :class="['tab',selectIndex===9&&'active']" @click="handleChangeTab(9)">VR虚拟演示</div>
     </div>
   </div>
   <div class="info">
@@ -32,12 +32,22 @@
   import FooterDoc from '../../components/footer-doc'
     export default {
         name: "monitoringDate",
+        data(){
+            return{
+                selectIndex:1
+            }
+        },
       components:{
         ToolBar,
         TableData,
         BottomData,
         FooterDoc
-      }
+      },
+        methods:{
+            handleChangeTab(selectIndex){
+                this.selectIndex=selectIndex
+            }
+        }
     }
 </script>
 
